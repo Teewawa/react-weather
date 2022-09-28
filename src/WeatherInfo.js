4c746a8bb96c4b4bb5e8de37beac13ed724493ba
Tiara Mendiola <dev.tiaram@gmail.com>
2022-09-28 17:27:14 -0500

    Added WeatherTemperature component
    and remove the JSX that previously displayed the temperature in celsius

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Weather.css";
 import FormatDate from "./FormatDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -19,10 +20,7 @@ export default function WeatherInfo(props) {
             code={props.data.icon}
             alt={props.data.description}
           />
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <span className="unit">°C</span>
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6">
           <ul>
