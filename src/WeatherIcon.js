89e0f759e51d9aba7227348ad51f0444da031c69
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-03 09:29:27 -0500

    Updated Icon property value
    Changed size value to props.size to have control over the weather icon's size being displayed in different parts of the page

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherInfo(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#1e1e1e"
-      size={55}
+      size={props.size}
       animate={true}
     />
   );
