6afc6b411994be95956c79baa301ee0e27554980
Tiara Mendiola <dev.tiaram@gmail.com>
2022-10-03 12:28:18 -0500

    Created search function & useEffect
    Moved api call into search function. Used useEffect hook, to set loaded to false when props.coordinates updates to new coordinates

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -8,29 +8,46 @@ export default function WeatherForecast(props) {
   const [forecast, setForecast] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
-  if (loaded) {
-    return (
-      <div className="WeatherForecast">
-        <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
-        </div>
-      </div>
-    );
-  } else {
+  //useEffect(function(){ });
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function search() {
     let apiKey = "992334cb342dbc8013e1ddd308aa9e3d";
     let endpoint = "https://api.openweathermap.org/data/2.5/onecall";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `${endpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
+  }
 
-    return null;
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index > 0 && index < 6) {
+              return (
+                <div
+                  className="col"
+                  key={index}
+                >
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    search();
   }
 }
